82dec9893a4dc9461c1b701981668da052780a87
Guilherme <guilhermemourelhe@gmail.com>
2023-02-04 21:32:56 -0300

    novo cadastro

--- newcadastro.php
+++ newcadastro.php
@@ -1,106 +1,94 @@
 <?php
-//Iniciando a $_SESSION - Sempre que usarmos ela devemos iniciar a mesma.
+
 session_start();
 
-// Fazendo a verificação de segurança na $_SESSION no índice que criamos para não ter erros no script caso ela não seja setada.
 if (!isset($_SESSION["User"])) {
     $_SESSION["User"] = array();
 }
 
-//Destruindo os dados armazenados na SESSION 
+if (isset($_POST["Enviar"])) {
+
+    $nome = isset($_POST["username"]) ? $_POST["username"] : "";
+    $data = isset($_POST["data"]) ? $_POST["data"] : "";
+    $quartos = isset($_POST["qtdquartos"]) ? $_POST["qtdquartos"] : "";
+    $televisores = isset($_POST["qtdtv"]) ? $_POST["qtdtv"] : "";
+
+    $dataUS = explode("-", $data);
+    $dataBR = [""];
+
+    if (count($dataUS) > 1) {
+        for ($i = 0, $j = 2; $j >= 0; $i++, $j--) {
+            $dataBR[$i] = $dataUS[$j];
+        }
+        $data = implode("-", $dataBR);
+    }
+
+
+    $cadastro = ["nome" => $nome, "dataNasc" => $data, "qtdquartos" => $quartos, "qtdtelevisor" => $televisores];
+
+
+    $error = false;
+
+    foreach ($_SESSION["User"] as $ID => $registro) {
+        if ($registro["nome"] == $nome) {
+            $error = true;
+            echo "Esse nome ja foi cadastrado anteriormente!";
+        }
+    }
+
+    if ($error == false) {
+        array_push($_SESSION["User"], $cadastro);
+        echo "Pessoa cadastrada com sucesso!";
+    }
+}
+
 if (isset($_POST["Limpar"])) {
     session_destroy();
     session_start();
-    echo "Banco de dados apagado :)";
+    echo "Os registros foram apagados com sucesso!";
 }
-
-if (isset($_POST["Enviar"])) {
-
-    $nome = isset($_POST["username"]) ? $_POST["username"] : '';
-    $date = isset($_POST["data"]) ? $_POST["data"] : '';
-    $quartos = isset($_POST["qtdquartos"]) ? $_POST["qtdquartos"] : '';
-    $tv = isset($_POST["qtdtv"]) ? $_POST["qtdtv"] : '';
-
-    // transforma a data para o formato brasileiro.
-    $dataUS = explode("-", $date);
-    $databr = array();
-
-    if (count($dataUS) > 1) {
-        for ($i = 2, $j = 0; $i >= 0; $i--, $j++) {
-            $databr[$j] = $dataUS[$i];
-        }
-
-        $date = implode("-", $databr);
-    }
-
-    // Juntandos todos os dados do cadastro em único array
-    $pessoa = ["nome" => $nome, "data" => $date, "quarto" => $quartos, "televisores" => $tv];
-
-    //variável de controle que informa se pessoa já tiver sido cadastrada.
-    $erro = false;
-
-    foreach ($_SESSION["User"] as $cadastro) {
-        if ($cadastro["nome"] == $nome) {
-            $erro = true;
-            echo "Essa pessoa já foi cadastrada anteriormente.";
-            break;
-        }
-    }
-
-    if ($erro != true) {
-        array_push($_SESSION["User"], $pessoa);
-        echo "Cadastro efetuado com sucesso!";
-    }
-}
-
 
 if (isset($_POST["Listar"])) {
 
-    if (count($_SESSION["User"]) == 0) {
-        echo "Nenhuma pessoa foi cadastrada até o momento...";
-    }
-
     foreach ($_SESSION["User"] as $ID => $registro) {
-
-        //  print_r($registro);
-        echo $ID . " = " .  $registro["nome"] . " - " . $registro["data"] . " quartos: " . $registro["quarto"] . " televisões: " . $registro["televisores"];
+        echo $ID . " -- " . " Nome: " . $registro["nome"] . " - " . " Nascimento: " . $registro["dataNasc"] .  " - " . " quartos: " . $registro["qtdquartos"] .  " - " . " televisões: " . $registro["qtdtelevisor"];
         echo "<br>";
     }
 }
 
 if (isset($_POST["excluirBtn"])) {
 
-    $excluir_id = isset($_POST["excluirPessoa"]) ? $_POST["excluirPessoa"] : '';
+    $excluirID = isset($_POST["excluirPessoa"]) ? $_POST["excluirPessoa"] : "";
 
-    foreach ($_SESSION["User"] as $ID => $valor) {
-
-        if ($ID == $excluir_id) {
-            unset($_SESSION["User"][$excluir_id]);
+    foreach ($_SESSION["User"] as $ID => $registro) {
+        if ($ID == $excluirID) {
+            unset($_SESSION["User"]["$excluirID"]);
+            echo "Registro excluído com sucesso!";
         }
     }
 }
-
 if (isset($_POST["consultarIdBtn"])) {
 
-    $consulta_id = isset($_POST["consultarId"]) ? $_POST["consultarId"] : '';
+    $consultaID = isset($_POST["consultarId"]) ? $_POST["consultarId"] : "";
 
-    foreach ($_SESSION["User"] as $ID => $valor) {
-        if ($ID == $consulta_id) {
-            echo $ID . " = " .  $valor["nome"] . " - " . $valor["data"] . " quartos: " . $valor["quarto"] . " televisões: " . $valor["televisores"];
+    foreach ($_SESSION["User"] as $ID => $registro) {
+        if ($ID == $consultaID) {
+            echo $ID . " -- " . " Nome: " . $registro["nome"] . " - " . " Nascimento: " . $registro["dataNasc"] .  " - " . " quartos: " . $registro["qtdquartos"] .  " - " . " televisões: " . $registro["qtdtelevisor"];
         }
     }
 }
 
 if (isset($_POST["consultarNmBtn"])) {
 
-    $consulta_nome = isset($_POST["consultarNome"]) ? $_POST["consultarNome"] : '';
+    $consultaNome = isset($_POST["consultarNome"]) ? $_POST["consultarNome"] : "";
 
-    foreach ($_SESSION["User"] as $ID => $valor) {
-        if ($valor["nome"] == $consulta_nome) {
-            echo $ID . " = " .  $valor["nome"] . " - " . $valor["data"] . " quartos: " . $valor["quarto"] . " televisões: " . $valor["televisores"];
+    foreach ($_SESSION["User"] as $ID => $registro) {
+        if ($registro["nome"] == $consultaNome) {
+            echo $ID . " -- " . " Nome: " . $registro["nome"] . " - " . " Nascimento: " . $registro["dataNasc"] .  " - " . " quartos: " . $registro["qtdquartos"] .  " - " . " televisões: " . $registro["qtdtelevisor"];
         }
     }
 }
+
 
 ?>
 
@@ -116,7 +104,7 @@ if (isset($_POST["consultarNmBtn"])) {
 
 <body>
     <div class="Main">
-        <form name="cadastroIbge" id=cadastroIbge action="cadastro.php" method="POST">
+        <form name="cadastroIbge" id=cadastroIbge action="newcadastro.php" method="POST">
             <h2> CADASTRO IBGE </h2>
 
             <div class="userInput">
@@ -156,12 +144,13 @@ if (isset($_POST["consultarNmBtn"])) {
                 <input type="submit" name="Listar" id="Listar" value="Listar">
 
             </div>
+
             <br>
 
             <div class="userInput">
                 <label> Excluir registro por id: </label>
                 <br>
-                <input type="number" name="excluirPessoa" id="excluirPessoa" min="0" size="25">
+                <input type="number" name="excluirPessoa" id="excluirPessoa" min="0" size="25" max="<?php echo count($_SESSION["User"]) ?>">
                 <input type="submit" name="excluirBtn" id="excluirBtn" value="Excluir">
             </div>
             <br>
@@ -169,7 +158,7 @@ if (isset($_POST["consultarNmBtn"])) {
             <div class="userInput">
                 <label> Consultar registro por id: </label>
                 <br>
-                <input type="number" name="consultarId" id="consultarId" min="0" size="25">
+                <input type="number" name="consultarId" id="consultarId" min="0" size="25" max="<?php echo count($_SESSION["User"]) ?>" >
                 <input type="submit" name="consultarIdBtn" id="consultarIdBtn" value="Consultar Id">
             </div>
             <br>
